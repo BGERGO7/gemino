d49fdb8181ab4976dd691011de1af845341d9d51
BGERGO7 <38499123+BGERGO7@users.noreply.github.com>
2022-08-04 22:04:19 +0200

    addid dots under slides

--- slide.js
+++ slide.js
@@ -1,17 +1,26 @@
 var slideIndex = 1;
-    showDivs(slideIndex);
+showDivs(slideIndex);
     
-    function plusDivs(n) {
-      showDivs(slideIndex += n);
-    }
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
-    function showDivs(n) {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length}
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-      }
-      x[slideIndex-1].style.display = "block";  
+function currentSlide(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
